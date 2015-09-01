ffe056aa63e0cb113c68ed9149c899ac3cb7affa
Peter Boerboom <plboerboom@gmail.com>
2015-09-01 12:58:23 -0400

    Organize for build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 
-var jshint = require('gulp-jshint');
+var alljs = 'src/javascript/**/*.js';
+
+var jshint = require('gulp-jshint'),
+    concat = require('gulp-concat');
 
 gulp.task('lint', function() {
-    return gulp.src('src/javascript/**/*.js')
-                         .pipe(jshint())
-                         .pipe(jshint.reporter('default'));
+    return gulp.src(alljs)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
 gulp.task('copyhtml', function() {
@@ -14,4 +17,10 @@ gulp.task('copyhtml', function() {
 
 //gulp.watch('src/javascript/**/*.js', ['lint']);
 
-gulp.task('default', ['lint', 'copyhtml']);
+gulp.task('build-js', function() {
+    return gulp.src(alljs)
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('public/javascript'))
+} )
+
+gulp.task('default', ['lint', 'copyhtml', 'build-js']);
