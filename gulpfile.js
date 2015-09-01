1a52d10cf68e11e1372a7889c7f37a849a423ade
Peter Boerboom <plboerboom@gmail.com>
2015-09-01 12:16:24 -0400

    WIP gulp build.

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,15 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
 gulp.task('lint', function() {
-    return gulp.src('src/js/*.js')
+    return gulp.src('src/javascript/**/*.js')
                          .pipe(jshint())
                          .pipe(jshint.reporter('default'));
 });
 
-gulp.task('default', ['lint']);
+gulp.task('copyhtml', function() {
+    gulp.src('src/*.html').pipe(gulp.dest('public'));
+});
+
+//gulp.watch('src/javascript/**/*.js', ['lint']);
+
+gulp.task('default', ['lint', 'copyhtml']);
