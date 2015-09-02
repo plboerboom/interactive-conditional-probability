89176901c0342846e0e6396e479753e03369df08
Peter Boerboom <plboerboom@gmail.com>
2015-09-02 13:14:08 -0400

    Drawable event objects.

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -6,25 +6,6 @@
     var universeTop = universeHeight / 2;
     var universeLeft = leftPadding;
 
-    var eventA = {
-        probability: 0.2,
-        color: "rgba(0, 0, 255, 0.1)" 
-    };
-
-    var eventNotA = {
-        probability: 1 - eventA.probability,
-        color: "rgba(0, 0, 255, 0.5)"
-    };
-
-    var eventBGivenA = {
-        probability: 0.9,
-        color: "rgba(0, 255, 0, 0.3)"
-    };
-
-    var eventBGivenNotA = {
-        probability: 0.5,
-        color: "rgba(0, 255, 0, 0.3)"
-    };
 
     var probA = 0.05;
     var probBGivenA = 0.9;
@@ -71,53 +52,115 @@
         window.requestAnimFrame(draw);
     }
 
+    var eventA = {
+        probability: 0.2,
+        left: 0,
+        top: 0,
+        get width() {
+            return this.probability * universeWidth;
+        },
+        height: universeHeight,
+        color: "rgba(0, 0, 255, 0.1)"
+    };
+
+    var eventNotA = {
+        get probability() {
+            return 1 - eventA.probability;
+        },
+        get left() {
+            return eventA.width;
+        },
+        top: 0,
+        get width() {
+            return universeWidth - eventA.width;
+        },
+        height: universeHeight,
+        color: "rgba(0, 0, 255, 0.5)"
+    };
+    console.log(eventNotA.left);
+
+    var eventBGivenA = {
+        probability: 0.8,
+        get left() {
+            return eventA.width - this.width;
+        },
+        top: 0,
+        get width() {
+            return this.probability * eventA.width;
+        },
+        height: universeHeight,
+        color: "rgba(0, 255, 0, 0.3)"
+    };
+
+    var eventBGivenNotA = {
+        probability: 0.5,
+        get left() {
+            return eventA.width;
+        },
+        top: 0,
+        get width() {
+            return this.probability * eventNotA.width;
+        },
+        height: universeHeight,
+        color: "rgba(0, 255, 0, 0.3)"
+    };
+
+    function drawEvent(e) {
+        context.save();
+        context.fillStyle = e.color;
+        console.log(e.left);
+        context.translate(leftPadding, universeTop);
+        context.fillRect(e.left, e.top, e.width, e.height);
+        context.restore();
+    }
 
     function draw() {
-        var probAWidth = probA * universeWidth;
-        var probACompWidth = universeWidth - probAWidth;
-        var probBGivenAWidth = probAWidth * probBGivenA;
-        var probBGivenNotAWidth = probBGivenNotA * probACompWidth;
-
         context.clearRect(0, 0, canvas.width, canvas.height);
 
-        context.save();
-        context.fillStyle = probAColor;
-        context.fillRect(universeLeft, universeTop, probAWidth, universeHeight);
-        context.strokeStyle = 'black';
-        context.beginPath();
-        context.moveTo(universeLeft, universeTop - 10);
-        context.lineTo(universeLeft + 5, universeTop - 20);
-        context.lineTo(universeLeft + probAWidth - 5, universeTop - 20);
-        context.lineTo(universeLeft + probAWidth, universeTop - 10);
-        context.stroke();
-        context.closePath();
-        context.restore();
-        context.save();
-        context.translate(probAWidth / 2, 0);
-        context.beginPath();
-        context.moveTo(0, 0);
-        context.lineTo(0, 200);
-        context.stroke();
-        context.closePath();
-        context.restore();
+//        var probAWidth = probA * universeWidth;
+//        var probACompWidth = universeWidth - probAWidth;
+//        var probBGivenAWidth = probAWidth * probBGivenA;
+//        var probBGivenNotAWidth = probBGivenNotA * probACompWidth;
 
-        context.save();
-        context.fillStyle = probACompColor;
-        context.translate(probAWidth, 0);
-        context.fillRect(universeLeft, universeTop, probACompWidth, universeHeight);
-        context.restore();
 
-        context.fillStyle = probBColor;
-
-        context.save();
-        context.translate(probAWidth - probBGivenAWidth, 0);
-        context.fillRect(universeLeft, universeTop, probBGivenAWidth, universeHeight);
-        context.restore();
-
-        context.save();
-        context.translate(probAWidth, 0);
-        context.fillRect(universeLeft, universeTop, probBGivenNotAWidth, universeHeight);
-        context.restore();
+//        context.save();
+//        context.fillStyle = probAColor;
+//        context.fillRect(universeLeft, universeTop, probAWidth, universeHeight);
+//        context.strokeStyle = 'black';
+//        context.beginPath();
+//        context.moveTo(universeLeft, universeTop - 10);
+//        context.lineTo(universeLeft + 5, universeTop - 20);
+//        context.lineTo(universeLeft + probAWidth - 5, universeTop - 20);
+//        context.lineTo(universeLeft + probAWidth, universeTop - 10);
+//        context.stroke();
+//        context.closePath();
+//        context.restore();
+//        context.save();
+//        context.translate(probAWidth / 2, 0);
+//        context.beginPath();
+//        context.moveTo(0, 0);
+//        context.lineTo(0, 200);
+//        context.stroke();
+//        context.closePath();
+//        context.restore();
+//
+//        context.save();
+//        context.fillStyle = probACompColor;
+//        context.translate(probAWidth, 0);
+//        context.fillRect(universeLeft, universeTop, probACompWidth, universeHeight);
+//        context.restore();
+//
+//        context.fillStyle = probBColor;
+//
+//        context.save();
+//        context.translate(probAWidth - probBGivenAWidth, 0);
+//        context.fillRect(universeLeft, universeTop, probBGivenAWidth, universeHeight);
+//        context.restore();
+//
+//        context.save();
+//        context.translate(probAWidth, 0);
+//        context.fillRect(universeLeft, universeTop, probBGivenNotAWidth, universeHeight);
+//        context.restore();
 
     }
 
